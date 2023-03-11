c0acf7e8240632dc068e362dffcdc83b8276d814
Preetambeeravelli <preetambeeravelli@gmail.com>
2023-03-11 15:40:28 -0600

    add page number in fetch data function

--- NewsSwiftUI/NetworkManager.swift
+++ NewsSwiftUI/NetworkManager.swift
@@ -12,8 +12,9 @@ class NetworkManager: ObservableObject{
     
     @Published var posts = [Post]()
     
-    func fetchData(){
-        guard let url = URL(string: "https://hn.algolia.com/api/v1/search_by_date?query=front_page") else {return}
+    func fetchData(pageNumber: Int){
+        guard pageNumber < 30 else {return}
+        guard let url = URL(string: "https://hn.algolia.com/api/v1/search_by_date?page=\(pageNumber)") else {return}
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
             guard let data = data else {return}
             do{
